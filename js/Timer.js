20f93e6f4ac17035d49b7fd7ce2e56822054bcd7
Capacity <lolzhenya10@gmail.com>
2025-01-07 19:45:17 +0200

    Implemented timer functionality. Added comments that describe the functions of Timer

--- js/Timer.js
+++ js/Timer.js
@@ -1,5 +1,5 @@
 export default class Timer {
-    constructor(root) {
+    constructor(root, initialMinutes = 0) {
         root.innerHTML = Timer.getHTML();
 
         this.el = {
@@ -10,19 +10,13 @@ export default class Timer {
         };
 
         this.interval = null;
-        this.remainingSeconds = 90;
+        this.remainingSeconds = initialMinutes * 60;
 
-        this.el.control.addEventListener("click", () => {
-            // TODO: Add in the code
-        });
+        this.el.control.addEventListener("click", () => this.toggle());
 
-        this.el.reset.addEventListener("click", () => {
-            // TODO: Add in the code
-        });
-
-        this.updateInterfaceTime();
+        this.el.reset.addEventListener("click", () => this.reset());
     }
-
+    // Update the timer display
     updateInterfaceTime() {
         const minutes = Math.floor(this.remainingSeconds / 60);
         const seconds = this.remainingSeconds % 60;
@@ -30,6 +24,63 @@ export default class Timer {
         this.el.seconds.textContent = seconds.toString().padStart(2, "0");
     }
 
+    // Update control button
+    updateInterfaceControls() {
+        if (this.interval === null) {
+            this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+            this.el.control.classList.add("timer__btn--start");
+            this.el.control.classList.remove("timer__btn--stop");
+        } else {
+            this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+            this.el.control.classList.add("timer__btn--stop");
+            this.el.control.classList.remove("timer__btn--start");
+        }
+    }
+
+    // Toggle between start and stop
+    toggle() {
+        if (this.interval === null) {
+            this.start();
+        } else {
+            this.stop();
+        }
+    }
+
+    // Reset the timer with user input
+    reset() {
+        const inputMinutes = prompt("Enter the number of minutes (0-59):");
+
+        if (inputMinutes < 60) {
+            this.stop();
+            this.remainingSeconds = inputMinutes * 60;
+            this.updateInterfaceTime();
+        }
+    }
+
+    // Start the timer
+    start() {
+        if (this.remainingSeconds === 0) return;
+
+        this.interval = setInterval(() => {
+            this.remainingSeconds--;
+            this.updateInterfaceTime();
+
+            if (this.remainingSeconds === 0) {
+                this.stop();
+            }
+        }, 1000);
+
+        this.updateInterfaceControls();
+    }
+
+    // Stop the timer
+    stop() {
+        clearInterval(this.interval);
+        this.interval = null;
+        this.updateInterfaceControls();
+    }
+
+    // HTML structure generating
     static getHTML() {
         return ` 
 			<span class="timer__part timer__part--minutes">00</span>
