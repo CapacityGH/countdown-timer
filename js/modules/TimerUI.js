b994ec007a31f3cfa25efed3f16590bc81cc0827
Capacity <lolzhenya10@gmail.com>
2025-01-07 21:50:28 +0200

    Changed the UI of the project. Added more CSS, HTML title and copyright. Added a wrapper for spans in TimerUI called timer-parts. Changed the screenshot of project in assest

--- js/modules/TimerUI.js
+++ js/modules/TimerUI.js
@@ -62,9 +62,11 @@ export default class TimerUI {
 
     static getHTML() {
         return ` 
-            <span class="timer__part timer__part--minutes">00</span>
-            <span class="timer__part">:</span>
-            <span class="timer__part timer__part--seconds">00</span>
+            <div class="timer-parts">
+                <span class="timer__part timer__part--minutes">00</span>
+                <span class="timer__part">:</span>
+                <span class="timer__part timer__part--seconds">00</span>
+            </div>
             <button type="button" class="timer__btn timer__btn--control timer__btn--start">
                 <span class="material-icons"> play_arrow </span>
             </button>
